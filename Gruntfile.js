d9bfbe83ad00ae1c4d1c7ba68b28df9cf142e846
Jonathan Beezley <jonathan.beezley@kitware.com>
2014-11-27 20:36:18 -0500

    Adding images from DataTables

--- Gruntfile.js
+++ Gruntfile.js
@@ -302,6 +302,12 @@ module.exports = function (grunt) {
           cwd: 'bower_components/bootstrap/dist',
           src: 'fonts/*',
           dest: '<%= config.dist %>'
+        }, {
+          expand: true,
+          dot: true,
+          cwd: 'bower_components/DataTables/media',
+          src: 'images/*',
+          dest: '<%= config.dist %>'
         }]
       },
       styles: {
